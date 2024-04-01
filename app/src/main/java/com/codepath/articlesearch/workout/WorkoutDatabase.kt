8b49461206e4224154028359fbbfff116d9920fb
Detective Pikachu <frenzyexists@gmail.com>
2024-04-01 02:42:59 -0400

    DB is fixed

--- app/src/main/java/com/codepath/articlesearch/workout/WorkoutDatabase.kt
+++ app/src/main/java/com/codepath/articlesearch/workout/WorkoutDatabase.kt
@@ -1,4 +1,32 @@
 package com.codepath.articlesearch.workout
 
-class WorkoutDatabase {
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.codepath.articlesearch.journal.JournalDAO
+
+@Database(entities = [Workout::class], version = 1)
+abstract class WorkoutDatabase : RoomDatabase() {
+    abstract fun getWorkoutDao(): WorkoutDAO
+    abstract fun getJournalDao(): JournalDAO
+
+    companion object {
+        @Volatile
+        private var INSTANCE: WorkoutDatabase? = null
+
+        fun getDatabase(context: Context): WorkoutDatabase {
+            return INSTANCE ?: synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    WorkoutDatabase::class.java,
+                    "workout_database"
+                ).build()
+
+                INSTANCE = instance
+                instance
+            }
+        }
+    }
+
 }
